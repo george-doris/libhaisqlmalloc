6042dcda7f03ab2370cd46062f04d6e1862a3e2b
郭忠明 <wlmqgzm@sohu.com>
2023-01-28 13:25:35 +0000

    haisql_memory_malloc_extend_define.hpp
    Signed-off-by: 郭忠明 <wlmqgzm@sohu.com>

---
+++ haisql_memory_malloc_extend_define.hpp
@@ -0,0 +1,17 @@
+#ifndef   _haisql_malloc_extend_define_hpp
+#define  _haisql_malloc_extend_define_hpp
+
+extern "C"
+{
+    //  下面的内容都是扩展函数
+    void     malloc_show_config( void )  noexcept;
+    size_t  malloc_all_size( void )  noexcept;
+    void     malloc_set_limit_callback( const size_t sizet_limit_size_in, void ( *ptr_callback_function_in )( size_t ) )  noexcept;
+    void     malloc_version( void ) noexcept;
+    unsigned int malloc_page_type( void* ptr_void_in )  noexcept;
+    void     malloc_multi( const unsigned int uint_size_in, const unsigned int uint_count_in, void** ptr_ptr_void_out  ) noexcept;
+    void     free_multi( void** ptr_ptr_void_in, const unsigned int uint_count_in )  noexcept;
+    void     memset_zero( void* ptr_void_in, const size_t sizet_size_in )  noexcept;
+
+}
+#endif // _haisql_malloc_extend_define_hpp
